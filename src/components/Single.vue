4d9fdac4a3fe183a54792216fa35afe9721aceca
Bob <bob_w@qq.com>
2020-11-05 18:19:10 +0800

    演示es6 generator的使用

---
+++ src/components/Single.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <button @click="next">next</button>
+    <p>{{ person }}</p>
+    <p>{{ done }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      person: "",
+      done: false,
+      personGenerator: null,
+    };
+  },
+  methods: {
+    next() {
+      if (!this.personGenerator) {
+        this.personGenerator = new this.PersonGenerator();
+      }
+      const result = this.personGenerator.next();
+      this.person = result.value;
+      this.done = result.done;
+    },
+    *PersonGenerator() {
+      yield "Bob";
+      yield "Tom";
+      yield "Jack";
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
