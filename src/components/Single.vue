e3f01e538349e77e2014c85b66a293c45b84f059
Bob <bob_w@qq.com>
2020-11-06 19:21:18 +0800

    向生成器传值

--- src/components/Single.vue
+++ src/components/Single.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <input type="text" v-model="be" />
     <button @click="next">next</button>
     <p>{{ person }}</p>
     <p>{{ done }}</p>
@@ -13,22 +14,24 @@ export default {
       person: "",
       done: false,
       personGenerator: null,
+      be: "",
     };
   },
   methods: {
     next() {
       if (!this.personGenerator) {
-        this.personGenerator = this.PersonGenerator();
+        this.personGenerator = this.PersonGenerator("Jump");
       }
-      const result = this.personGenerator.next();
+      const result = this.personGenerator.next(this.be);
       this.person = result.value;
       this.done = result.done;
     },
-    *PersonGenerator() {
-      yield "Bob";
-      yield "Tom";
-      yield* this.ToolGenerator();
-      yield "Jack";
+    *PersonGenerator(action) {
+      const p1 = yield "Bob " + action;
+      console.log(`p1: ${p1}`);
+      const p2 = yield "Jack " + action;
+      console.log(`p2: ${p2}`);
+      yield `Tom(${p1})` + action;
     },
     *ToolGenerator() {
       yield "刀子";
