d81d3ced5a5a3375bc2178d539e4a2490d546aa6
Bob <bob_w@qq.com>
2020-11-08 22:20:50 +0800

    使用js代码进行页面跳转

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,6 +2,7 @@
   <div class="about">
     <h1>This is an about page</h1>
     <!-- 获取路由路径参数 -->
-    <h3>{{ $route.params.msg }}</h3>
+    <h3>msg:{{ $route.params.msg }}</h3>
+    <h3>id:{{ $route.params.id }}</h3>
   </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,30 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <button @click="goAbout">跳转到about</button>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+  methods: {
+    goAbout() {
+      this.$router.push({
+        name: "About",
+        params: {
+          id: 1,
+          msg: "from home",
+        },
+      });
+    },
+  },
+};
 </script>
